61fc9ab4282937569a438b2b616a3e1033a823a1
rimaulana <rimaulana@outlook.com>
2016-08-01 16:19:47 +0700

    Turbo worker checking

--- dashboard.sh
+++ dashboard.sh
@@ -2,13 +2,18 @@
 
 SCRIPT=$(readlink -f $0)
 ROOTDIR=`dirname $SCRIPT`
+pingInterval=10
+pingTimer=0
 
 while true
 do
-    if ping -q -c 1 -W 1 www.google.com >/dev/null; then
-        Connected=true
-    else
-        Connected=false
+    if [[ $((pingTimer)) > $((pingTimer)) ]]; then
+        if ping -q -c 1 -W 1 www.google.com >/dev/null; then
+            Connected=true
+        else
+            Connected=false
+        fi
+        pingTimer=0
     fi
     dispatcherPID=`cat $ROOTDIR/dispatcher.pid`
     poolFiles=`ls $ROOTDIR/pool/ | wc -l`
@@ -43,9 +48,13 @@ do
     echo -e "Total active worker: \e[96m$((activeWorker - 1))\e[0m"
     echo -e "Progress           : \e[96m$resultFiles\e[0m out of \e[93m$totalFiles\e[0m"
     echo -e "Total worked files : \e[96m$workedFiles\e[0m"
-    echo -e "                   : \e[92m[\e[96m$lineProgress\e[92m] \e[96m$percentage\e[92m%\e[0m"
+    echo -e ""
+    #echo -e "                   : \e[92m[\e[96m$lineProgress\e[92m] \e[96m$percentage\e[92m%\e[0m"
+    $ROOTDIR/progress.sh 40 $resultFiles $totalFiles
+    #echo -e "[01234567890123456789012345678901234567890]"
     echo -e " "
-    echo -e "     \e[92mPID\tELAPSED(s)\tWORKED FILE\e[0m"
-    ps ax -o pid,etimes,cmd | grep worker.py | awk '{if($3 == "python"){print "     \033[93m"$1"\t\033[96m"$2"\t\t\033[93m"$5"\033[0m"}}'
+    echo -e "  \e[92mPID\tELAPSED(s)\tWORKED FILE\e[0m"
+    ps ax -o pid,etimes,cmd | grep worker.py | awk '{if($3 == "python"){print "  \033[93m"$1"\t\033[96m"$2"\t\t\033[93m"$5"\033[0m"}}'
+    ((pingTimer=pingTimer+1))
     sleep 1
 done
--- dispatcher.sh
+++ dispatcher.sh
@@ -3,34 +3,31 @@
 SCRIPT=$(readlink -f $0)
 ROOTDIR=`dirname $SCRIPT`
 maxActiveWorker=8
+pingInterval=10
+pingTimer=0
 
 echo "$$" > $ROOTDIR/dispatcher.pid
 while true
 do
-    if ping -q -c 1 -W 1 www.google.com >/dev/null; then
-        Connected=true
-    else
-        Connected=false
+    if [[ $((pingTimer)) > $((pingTimer)) ]]; then
+        if ping -q -c 1 -W 1 www.google.com >/dev/null; then
+            Connected=true
+        else
+            Connected=false
+        fi
+        pingTimer=0
     fi
     if $Connected; then
-        poolCount=`ls $ROOTDIR/pool/ | wc -l`
-        if [[ $((poolCount)) > 0 ]]; then
-            activeWorker=`ps aux | grep worker.py | wc -l`
-            newSpawn=$((maxActiveWorker - activeWorker + 1))
-            #poolFiles=($(ls $ROOTDIR/pool/))
-            #for file in ${poolFiles[@]}
-            #do
-                #if [[ $((newSpawn)) > 0 ]]; then
-            for (( c=$newSpawn; c>0; c--))
+        activeWorker=`ps ax -o cmd | grep worker.py | awk '{if($1 == "python"){print $1}}' | wc -l`
+        newSpawn=$((maxActiveWorker - activeWorker ))
+        if [[ $((newSpawn)) > 0 ]]; then
+            list=($(ls $ROOTDIR/pool/ | head -$newSpawn))
+            for file in ${list[@]}
             do
-                file=`ls -la $ROOTDIR/pool/ | awk 'NR==4 {print $9}'`
                 mv $ROOTDIR/pool/$file $ROOTDIR/worked/$file
                 nohup python $ROOTDIR/worker.py $file &
             done
-                    #newSpawn=$((newSpawn - 1))
-                #fi
-            #done
         fi
     fi
-    sleep 3
+    ((pingTimer=pingTimer+1))
 done
